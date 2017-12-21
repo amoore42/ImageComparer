57f100c064f02103c49fab0dc605cfe085eb8ef1
djaymore <djaymore@hotmail.com>
2017-12-21 09:53:17 -0500

    using a cache along with the tree so that we don't have to keep loading tree

--- Managers/dataManager.js
+++ Managers/dataManager.js
@@ -4,7 +4,11 @@ var Picture = require('../models/pictureModel');
 
 var dataManager = function(postedPicture){
 
+    var CACHE_MAX_LENGTH = 100;
+    var HASH_DISTANCE = 0;
     var vpTreeData = "";
+    //The tree has to be rebuilt for each new element added.  Let's have an array to help augment the data
+    var preTreeArray = new Array();
     var postedPicture;
 
     var setPostedPicture = function(postedPicture){
@@ -19,15 +23,14 @@ var dataManager = function(postedPicture){
         picture.UserName = this.postedPicture.UserName;
 
         //Find the hash in the vpTree and close matches
-        var maximumDistance = 0;
-        var matches = this.getMatches(hash, maximumDistance);
+        var matches = this.getMatches(hash);
 
         //Nothing was found
         if(matches.length == 0){
-            picture.save(function(err){
+            picture.save(function(err, saved){
                 if(!err){
                     //reload the tree.  There is no method to just insert a record *yet
-                    loadAllPictures();
+                    cachePicture(saved.Hash);
                 }
             });
             returnValues.push(picture);
@@ -38,9 +41,10 @@ var dataManager = function(postedPicture){
             var index = matches.indexOf(hash);
             if(index == -1){
                 returnValues.push(picture);
-                picture.save(function(err){
-                    if(!err)
-                        loadAllPictures();
+                picture.save(function(err, saved){
+                    if(!err){
+                        cachePicture(saved.Hash);
+                    }
                 });
             }
 
@@ -60,7 +64,7 @@ var dataManager = function(postedPicture){
         var stepValue = 100;
         var allResults = new Array();
         Picture.findPaged({}, 'Hash', {step: stepValue}, function(results){
-            for(var i = 0; i < stepValue; ++i)
+            for(var i = 0; i < results.length; ++i)
                 allResults.push(results[i].Hash);
             return Promise.resolve();
         }).then(function(){
@@ -70,19 +74,38 @@ var dataManager = function(postedPicture){
         });
     };
 
-    var getMatches = function(hash, maximumDistance){
+    var getMatches = function(hash){
         //The two signifies the number of closest elements.  5 is the distance/tolerance
         var searchResult = vpTreeData.search(hash, 2, 5);
         var result = new Array();
 
         for(var i = 0; i < searchResult.length; ++i){
-            if(searchResult[i].d <= maximumDistance){
+            if(searchResult[i].d <= HASH_DISTANCE){
                 result.push(vpTreeData.S[searchResult[i].i]);
             }
+        }
+
+        //Now let's search items in the array and add them as well
+        for(var i = 0; i < preTreeArray.length; ++i){
+            var distance = leven.getEditDistance(hash, preTreeArray[i]);
+            if(distance <= HASH_DISTANCE)
+                result.push(preTreeArray[i]);
         }
         
         return result;
     };
+
+    //When adding new pictures we don't want to load the tree every time.  Instead an array is kept locally
+    //and used to search over.  Because the length is limited the search won't be slow.  However the tree is
+    //used to search the rest of the items.
+    var cachePicture = function(hash){
+        if(preTreeArray.length <= CACHE_MAX_LENGTH)
+            preTreeArray.push(hash);
+        else{
+            preTreeArray.length = 0;
+            loadAllPictures();
+        }
+    }
 
     return {
         setPostedPicture: setPostedPicture,
